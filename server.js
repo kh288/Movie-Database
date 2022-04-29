0d0c73d428cd8733ed7b9fe22c12f0f892e851c6
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-29 13:06:41 -0700

    Added setup for server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Express Routes
+// post '/api/new-movie'
+// get '/api/movies'
+// get '/api/movie-reviews'
+// put '/api/review/:id'
+// delete '/api/movie/:id'
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'courses_db'
+  },
+  console.log(`Connected to the courses_db database.`)
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
